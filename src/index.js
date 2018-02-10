72c0125163fe68f46ff31d822302fd9ae0e22db3
Josh Hunt <josh@trtr.co>
2018-02-10 13:50:41 +0000

    No service workers

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Router from './Router';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Router />, document.getElementById('root'));
 registerServiceWorker();
