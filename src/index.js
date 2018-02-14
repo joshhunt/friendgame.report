8903e627fdfbecec6a561dc7bdafa45543b80542
Josh Hunt <josh@trtr.co>
2018-02-14 21:20:44 +0000

    cool

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Router from './Router';
+import './autotrack.build';
 
 ReactDOM.render(<Router />, document.getElementById('root'));
 
